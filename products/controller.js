28bff8c16508c8f628ea4272703073e231270f9f
Jhonatan Alarcón <jhonatan.alarcon.ariza@gmail.com>
2022-03-07 17:33:12 -0500

    add getProduct by id

--- products/controller.js
+++ products/controller.js
@@ -1,5 +1,6 @@
 const products = [
   {
+    id: 1,
     name: 'Samnsung A20',
     price: 200,
     currency: { name: 'US Dollar', sign: '$' },
@@ -9,5 +10,9 @@ const products = [
 function getProducts() {
   return products;
 }
+function getProductById(id) {
+  console.log('log', id);
+  return products.find((product) => product.id === Number(id));
+}
 
-module.exports = { getProducts };
+module.exports = { getProducts, getProductById };
--- products/router.js
+++ products/router.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { getProducts } = require('./controller');
+const { getProducts, getProductById } = require('./controller');
 
 /* GET product listing */
 router.get('/', async function (req, res, next) {
   const products = await getProducts();
   res.json(products);
+});
+/* GET product by id */
+router.get('/:id', async function (req, res, next) {
+  const id = req.params.id;
+  console.log(req.params);
+  const product = await getProductById(id);
+  res.json(product);
 });
 
 module.exports = router;
