a3dea928adad55d97bcf10a0222137ccd0dfac0f
Jhonatan Alarcón <jhonatan.alarcon.ariza@gmail.com>
2022-03-06 19:41:07 -0500

    Add axios implememtation

--- products/controller.js
+++ products/controller.js
@@ -1,12 +1,10 @@
-const products = [
-  {
-    name: 'Samnsung A20',
-    price: 200,
-    currency: { name: 'US Dollar', sign: '$' },
-  },
-];
+const axios = require('axios');
+const apiUrl =
+  'https://gist.githubusercontent.com/jhonatan89/719f8a95a8dce961597f04b3ce37f97b/raw/4b7f1ac723a14b372ba6899ce63dbd7c2679e345/products-ecommerce';
 
-function getProducts() {
+async function getProducts() {
+  const response = await axios.get(apiUrl);
+  const products = response.data;
   return products;
 }
 
