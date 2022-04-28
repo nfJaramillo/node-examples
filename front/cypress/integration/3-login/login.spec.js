edc6d6c04ed74f3f02977248fac9929dca60b9ab
Nicolás F. Jaramillo <nf.jaramillo@uniandes.edu.co>
2022-04-28 13:55:26 -0500

    Pruebas para inicio de sesion correcto e incorrecto

--- front/cypress/integration/3-login/login.spec.js
+++ front/cypress/integration/3-login/login.spec.js
@@ -21,11 +21,30 @@ describe('Login', () => {
   });
 
   it('displays form', () => {
-    cy.get('#username').type('isis3710');
-    cy.get('#password').type('isis3710');
+    cy.get('#username').type('test');
+    cy.get('#password').type('12345');
 
     cy.get('.btn.btn-primary').click();
-    //TODO expect here
+    cy.get('#user-data').should('not.have.text', 'No data');
+  });
+  //Add new test
+});
+
+describe('Wronng Login', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('/login');
+  });
+
+  it('displays form', () => {
+    cy.get('#username').type('test');
+    cy.get('#password').type('1234');
+
+    cy.get('.btn.btn-primary').click();
+    cy.get('#user-data').should('have.text', 'No data');
   });
   //Add new test
 });
